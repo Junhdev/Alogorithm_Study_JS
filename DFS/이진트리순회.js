40bfe509fc08bfc605546a2dbf55859acd5ece8e
이준형 <dlwnsgud261@naver.com>
2022-12-01 23:44:24 +0900

    DFS3

---
+++ DFS/이진트리순회.js
@@ -0,0 +1,15 @@
+function solution(n){
+    let answer="";
+    function DFS(v){
+        if(v>7) return;
+        else{
+            answer+=(v+' ');
+            DFS(v*2);
+            DFS(v*2+1);
+        }
+    }
+    DFS(n);
+    return answer;
+}
+
+console.log(solution(1));
