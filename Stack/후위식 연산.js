1e7a1de7ff0b0fa5ab6fbd84b85d3d12cd924846
이준형 <dlwnsgud261@naver.com>
2023-01-28 01:24:25 +0900

    섹션6-page7 수정

--- Stack/후위식 연산.js
+++ Stack/후위식 연산.js
@@ -1,21 +1,53 @@
-            function solution(s){  
-                let answer;
-                let stack=[];
-                for(let x of s){
-                    if(!isNaN(x)) stack.push(Number(x));
-                    else{
-                        let rt=stack.pop();
-                        let lt=stack.pop();
-                        if(x==='+') stack.push(lt+rt);
-                        else if(x==='-') stack.push(lt-rt);
-                        else if(x==='*') stack.push(lt*rt);
-                        else if(x==='/') stack.push(lt/rt);
-                    }
-                }
-                answer=stack[0];
-                return answer;
+function solution(s){
+    let answer;
+    let stack = [];
+    for(let x of s){
+        if(!isNaN(x)){ // x가 숫자이면
+            stack.push(Number(x));
+        }
+        else{// x가 연산식이면
+            let rt = stack.pop();
+            let lt = stack.pop();
+            if(x==='+'){
+                let result1 = lt+rt;
+                stack.push(result1);
             }
+            else if(x==='*'){
+                let result2 = lt * rt;
+                stack.push(result2);
+            }
+            else if(x==='/'){
+                let result3 = lt / rt;
+                stack.push(result3);
+            }
+            else if(x==='-'){
+                let result4 = lt - rt;
+                stack.push(result4);
+            }
+        }
+    }
+    answer = stack.pop();
 
-            let str="352+*9-";
-            console.log(solution(str));
+    return answer;
+
+    /* for(let i=0; i<myArr.length; i++){
+        if(typeof(myArr[i]) ==="number"){
+            stack.push(myArr[i]);
+        }
+        else{
+            let cal = myArr[i];
+            let rt = stack.pop();
+            let lt = stack.pop();
+            if(cal==='+'){
+                let result = lt + rt
+            }
+        }
+    }
+
+    let a = 5+2
+    let b= 3*a
+    } */
+                    
+}
+console.log(solution('352+*9-')); // 연산식이 포함되어 있으므로 문자열로 입력해줘야한다
         
