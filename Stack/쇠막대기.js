191e9ea13b602c0542f7e25fdab9267b65b28a2f
이준형 <dlwnsgud261@naver.com>
2023-01-30 01:33:07 +0900

    섹션6-page6 중요수정

--- Stack/쇠막대기.js
+++ Stack/쇠막대기.js
@@ -1,3 +1,27 @@
+/* 나의 풀이 >>> 중요! */
+function solution(s){
+    let myArr = s.split('');
+    let answer=0;
+    let stick=0;
+    for(let i=0; i<myArr.length; i++){
+        if (myArr[i]==='(' && myArr[i+1] !== ')'){
+            stick += 1;
+        }
+        else if(myArr[i]==='(' && myArr[i+1] ===')'){ //쇠막대기를 자르는 레이저는 적어도 하나 존재하므로 이 순서가 밑 순서보다 항상 먼저 일어난다.
+            answer += stick;
+        }
+        else if(myArr[i]===')' && myArr[i-1] !== '(' ){ // 디버깅 하면서 수정 ( 결과값 확인하여 어디가 틀렸는 지 입력예시 단계적으로 대입해보면서 분석)
+            stick -= 1;
+            answer += 1;
+        }
+        
+    }
+    return answer;
+}
+console.log(solution('(((()(()()))(())()))(()())'));
+
+
+/*
 function solution(s){
     let answer=0;
     let stack=[];
@@ -15,3 +39,4 @@ function solution(s){
 
 let a="()(((()())(())()))(())";
 console.log(solution(a));
+*/
