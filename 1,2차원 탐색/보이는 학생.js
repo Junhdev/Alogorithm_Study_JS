4e489089eb2eb1f2c217fdee09045b524036c0ec
이준형 <dlwnsgud261@naver.com>
2023-02-08 00:46:12 +0900

    섹션2-page2 수정

--- 1,2차원 탐색/보이는 학생.js
+++ 1,2차원 탐색/보이는 학생.js
@@ -1,9 +1,20 @@
-function solution(s){
-    let arr = [130,135,148,140,145,150,150,153];
-    for(let i=0; i<arr.length; i++){
-        
+function solution(N, input){
+    let arr = input.split(' ').map((x)=>{return parseInt(x);});
+    let answer = 1;
+    let max = arr[0];
+    for(let i=1; i<N; i++){       // i=4
+        if(arr[i-1]<arr[i]){ // arr[0] < arr[1] // arr[3]<arr[4]
+            //max = arr[i]; // max = arr[1]
+            if(arr[i]>max){ //arr[1]>max  // arr[4] < arr[2]
+                answer++; //answer=2
+            }
+            max = arr[i]; // max = arr[1] // max = arr[2]
+        }
 
     }
-                    
+    return answer;
 }
-console.log(solution()); 
+
+let N = 8;
+let input = '130 135 148 140 145 150 150 153'
+console.log(solution(N, input)); 
