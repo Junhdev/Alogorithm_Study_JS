68fc6f81710a42f5704a8dddf00d659d90f30d6b
이준형 <dlwnsgud261@naver.com>
2023-02-10 23:59:11 +0900

    PCCP 모의고사1-#3

---
+++ PCCP 모의고사1/#3. 유전법칙.js
@@ -0,0 +1,9 @@
+function solution(queries) {
+    var answer = [];
+    Rr 이면  arr = [[RR,Rr,Rr,rr]]
+    부모 노드가 Rr일때
+    [[2,i]] === arr[2][i-1] === []
+    RR 이면 [RR,RR,RR,RR]
+    rr 이면 [rr,rr,rr,rr]
+    return answer;
+}
