9d9791f714b057506e7682551e5d832d5b19b0d7
이준형 <dlwnsgud261@naver.com>
2023-04-14 01:36:43 +0900

    삽입정렬 알고리즘

--- PCCP 모의고사1/#1. 외톨이 알파벳.js
+++ PCCP 모의고사1/#1. 외톨이 알파벳.js
@@ -4,8 +4,8 @@ function solution(input_string) {
     let stack = [];
     let myArr = [];
     stack.push(inputArr[0]);
-    for(let i=1; i<inputArr.length; i++){//
-        if(stack.includes(inputArr[i])){//stack['e'] // stack['e','d'] // stack['e','d','e']
+    for(let i=1; i<inputArr.length; i++){
+        if(stack.includes(inputArr[i])){// stack['e'] // stack['e','d'] // stack['e','d','e']
             if(inputArr[i-1] !== inputArr[i]){ // 'd' !== 'e'
                 myArr.push(inputArr[i]); // x // myArr['e'] // ... // myArr['e','d']
             } 
