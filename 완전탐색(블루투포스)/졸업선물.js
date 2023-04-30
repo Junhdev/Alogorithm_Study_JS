c33840cd4a573178bbcc985e65701c42d99e13a7
이준형 <dlwnsgud261@naver.com>
2023-04-30 23:57:47 +0900

    조합 다시 풀기

--- 완전탐색(블루투포스)/졸업선물.js
+++ 완전탐색(블루투포스)/졸업선물.js
@@ -1,8 +1,30 @@
-function solution(M, input) {
-    let inputArr = [[6,6],[2,2],[4,3],[4,5],[10,3]];
+function solution(N, inputArr) {
+    
+    
+    let sumInput = 0;
+    let stack2 = [];
+    let stack3 = [];
+    for(let i=0; i<N; i++){
+        for(let j=0; j<2; j++){
+            sumInput += inputArr[i][j];
+        }
+        stack2.push(sumInput);
+        sumInput = 0;
+    }
+    //let sortInput = stack2.sort((a,b)=>{return a-b;});
+    
+    for(let i=0; i<N; i++){
+        
+            let saleInput = inputArr[i][0]/2 + inputArr[i][1];
+            stack3.push(saleInput);
+            saleInput = 0;
+
+        }
+    //let sortInput2 = stack3.sort((a,b)=>{return a-b;});
+
     let stack = [];
     // 최대 몇명인지 경우의 수 따져봐야함
-    for(let i=0; i<inputArr.length; i++){
+    /* for(let i=0; i<inputArr.length; i++){
         // inputArr[0][0]/2
         sum += inputArr[i][0]/2;
         for(let j=0; j<M; j++){
@@ -16,38 +38,56 @@ function solution(M, input) {
             sum += inputArr[j][1];
             
         }
-    }
-    let tmp = Array.from({length:3},()=>0); // 카드 3장 뽑으므로(3칸짜리 배열 생성)
+    } */
+    //let tmp = Array.from({length:3},()=>0); // 카드 3장 뽑으므로(3칸짜리 배열 생성)
+    
+    let myArr1 = 0;
+    let myArr2 = 0;
+    let myArr3 = 0;
+    let answer = [];
+    let tmp = [];
+    let answer3=Number.MIN_SAFE_INTEGER;
     function DFS(L,s,sum){ // DFS(0,0) // DFS(1,13)
-        for(let N=M; i>=1; i--){
-            if(L===N){ //3장은 고정
-                if(sum<=28){
-                    stack.push(N,sum);
-                }
-            // answer2.push(tmp.slice()); // answer2 배열 디버깅 // slice >> tmp 원본을 보존하고 싶을때 사용
+        
+        
+       
+            if(L===4&& sum<=28){ //3장은 고정
+                
+                    //stack.push(N,sum);
+                    
+                
+            answer.push(tmp.slice()); // answer2 배열 디버깅 // slice >> tmp 원본을 보존하고 싶을때 사용
             // answer1.push(sum); //sum 배열 디버깅
-            
+                
             }
+            
             else{ //L===0 // L===1
-                for(let i=s; i<N; i++){
-                    myArr1 = inputArr[i][0]/2
+                for(let i=s; i<5; i++){
+
+                    
+                    tmp[L] = stack2[i]
+                    /*
+                    myArr1 = stack2[i]
                     for(let j=s; j<N; j++){
                         if(j===i){
                             continue;
                         }
                         else{
-                            myArr2 = inputArr[i][0];
+                            myArr2 += stack3[j];
                         } // L===2,i===3
-                        myArr3 = inputArr[j][1];
+                        
                             
-                    }
+                    }*/
                     //console.log(tmp);
-                    DFS(L+1, i+1, sum+card[i]); 
+                    DFS(L+1, i+1,sum+stack2[i]); 
                 }
             }
         }
-    DFS(0,0,0); // 꼭 호출 해주어야함
-}
+     // 꼭 호출 해주어야함
     
-let input =;
-console.log(solution(5, input));
+    DFS(0,0,0);
+    
+    return answer;
+}
+let inputArr = [[6,6],[2,2],[4,3],[4,5],[10,3]];
+console.log(solution(5, inputArr));
