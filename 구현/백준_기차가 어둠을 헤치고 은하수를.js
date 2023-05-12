61900b715f20a40a155172ab847b7da2f6fb046a
이준형 <dlwnsgud261@naver.com>
2023-05-13 00:42:35 +0900

    알고리즘 정리

--- 구현/백준_기차가 어둠을 헤치고 은하수를.js
+++ 구현/백준_기차가 어둠을 헤치고 은하수를.js
@@ -1,17 +1,18 @@
 function solution(N, M , input) {
-    // 빈 배열 여러개 선언 후 할당하기
+    
     let order =  input;
     
-
     //[Array(20), Array(20), Array(20), Array(20), Array(20)]
     // [[], [], [], [], [],...[]]
+    // ★ 0으로 된 2차원배열에 태우면 1 내리면 0 으로 할당!!
     let train = Array.from(Array(N), () => Array(20).fill(0))
    
     // sub
     let trainNum= 0;
     let tmp = Array.from(Array(N), () => Array(20).fill(0));
     //console.log(tmp)
-    /*
+
+/* 
 for(let i=0; i<order.length; i++){
     if(order[i][0]===1){
         trainNum = order[i][1];
@@ -53,6 +54,8 @@ for(let i=0; i<order.length; i++){
 */
 
 for(let i=0; i<order.length; i++){
+    // 모든조건 실행해야하므로 else if가 아닌 if문으로!!!
+    // order[i][1]-1번의 기차에 사람 태우기
     if(order[i][0]===1){
         
         trainNum = order[i][1]-1;
@@ -61,20 +64,22 @@ for(let i=0; i<order.length; i++){
         }
         
     }
-    // 기차번호 1로 바뀐 상태에서
+    // order[i][1]-1번의 기차에 사람 하차시키기
     if(order[i][0] === 2){
-        // 번호 같다면
+        
         trainNum = order[i][1]-1;
         if(train[trainNum][order[i][2]-1] === 1){
             train[trainNum][order[i][2]-1] = 0;
         }
     }
-    //console.log(train)
+    // 오른쪽으로 한칸씩 옮기고 첫번쨰 사람은 0으로 만들어주기!!!
     if(order[i][0] === 3){
         trainNum = order[i][1]-1;
         
         for(let j=1; j<=19; j++){
-            
+            //★ tmp를 활용하여(얕은복사) 이동시켜야함!!!
+            // tmp는 앞명령들 모두 끝난 후의 결과가 적용된 배열!!!
+            // 그래서 맨 하단에 할당!!
             train[trainNum][j] = tmp[trainNum][j-1]
            
         }
@@ -86,6 +91,7 @@ for(let i=0; i<order.length; i++){
         trainNum = order[i][1]-1;
        
         for(let j=19; j>=1; j--){
+            // 여기다가 하면 안됨!
             //let tmp = train.slice();
             train[trainNum][j-1] = tmp[trainNum][j]
             
@@ -94,22 +100,23 @@ for(let i=0; i<order.length; i++){
         train[trainNum][19] = 0;
     }
     // ★★★선언 위치(scope 설정 중요)
+    // tmp는 앞명령들 모두 끝난 후의 결과가 적용된 배열!!!
+    // 2차원배열의 얕은복사
     tmp = train.map((v)=>v.slice())
 }
 
+// ★배열끼리 비교는 문자열로 변환해서 하자!!!
 let compareArr = [];
 compareArr.push(train[0].join(''))
 for(let i=1; i<train.length; i++){
-    
-        if(!compareArr.includes(train[i].join('')) && train[i].join('') !== '00000000000000000000'){
-            compareArr.push(train[i].join(''))
-        }
-        
+    // 아무도 안탔어도 기차 지나갈 수 있다! 단, 문제 조건만 만족한다면
+    if(!compareArr.includes(train[i].join('')) /*&& train[i].join('') !== '00000000000000000000'*/){
+        compareArr.push(train[i].join(''))
+    }   
 }
 
 answer = compareArr.length
-console.log(train)
-    return answer
+return answer
     
 }
 
