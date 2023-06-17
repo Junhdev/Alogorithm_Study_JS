fbd4746708df545c46679fe499f2e278376edc6c
이준형 <dlwnsgud261@naver.com>
2023-06-18 01:28:57 +0900

    알고리즘 추가

--- 구현/포함배열.js
+++ 구현/포함배열.js
@@ -1,21 +1,77 @@
+/* 네이버 부스트캠프 예시문제 풀이 */
+// 풀이1 - Map 객체 활용
 function solution(arr){
     let answer = [];
     let arrMap = new Map();
     
-    let stack = [];
+    arrMap.set(arr[0],1)
     for(let i=1; i<arr.length; i++){
-        if(!arrMap.has(arr[i])){
-            arrMap.set(arr[i-1], 0)
-        }
+        
         
         if(arrMap.has(arr[i])){
             let before = arrMap.get(arr[i])
+            // 수정의 기능 >> key 순서 그대로
             arrMap.set(arr[i], before+1)
         }
+        else{
+            // 수정의 기능 >> key 순서 그대로
+            arrMap.set(arr[i], 1)
+        }
+    }
+
+    let flag = false;
+    for (const [key, value] of arrMap) {
+        if(value > 1){
+            answer.push(value)
+        }
+        if(value !== 1){
+            flag = true;
+        }
+    }
+    if(flag === false){
+        answer.push(-1)
     }
     
     
    
-    return arrMap;
+    return answer;
 }
-console.log(solution([1,2,3,3,3,3,4,4]))
+console.log(solution([1,2,4,3,4,3,4,3,2]))
+
+// 상태 이중 배열 활용
+
+function solution(arr){
+    let answer = [];
+    let stack = [];
+    stack.push([arr[0],1]);
+    let flag = false
+    for(let i=1; i<arr.length; i++){
+        for(let j=0; j<stack.length; j++){
+            if(stack[j][0] === arr[i]){
+                stack[j][1] ++
+                flag = true
+            }
+        }
+        if(flag === false){
+            stack.push([arr[i],1])
+        }
+
+    }
+    
+    let flag2 = false
+    for(let i=0; i<stack.length; i++){
+
+            if(stack[i][1] > 1){
+                answer.push(stack[i][1])
+                flag2 = true
+            }
+            
+        
+    }
+    if(flag2 === false){
+        answer.push(-1)
+    }
+   
+    return answer;
+}
+console.log(solution([1,2,4,3,4,3,4,3,2]))
